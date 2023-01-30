fa2a30b33b857b8f7dedeeef8a3408fee5c0325f
wselby-git <122949882+wselby-git@users.noreply.github.com>
2023-01-29 18:45:41 -0700

    Create linux_headers.c
    A header file containing a headers for c.

---
+++ linux_headers.c
@@ -0,0 +1,42 @@
+// Author: William Selby
+// a header file containing all c headers
+
+#ifndef SYSTEM_HEADERS_H 
+#define SYSTEM_HEADERS_H 
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <assert.h> 
+#include <math.h> 
+#include <stdint.h> 
+#include <stdbool.h> 
+#include <ctype.h> 
+#include <limits.h> 
+#include <errno.h> 
+#include <float.h> 
+#include <locale.h> 
+#include <wchar.h> 
+#include <time.h> 
+#include <signal.h> 
+#include <setjmp.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <sys/time.h> 
+#include <fcntl.h> 
+#include <dirent.h> 
+#include <pthread.h> 
+#include <sys/wait.h> 
+#include <sys/mman.h> 
+#include <sys/ipc.h> 
+#include <sys/sem.h> 
+#include <sys/shm.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <netdb.h> 
+#include <sys/select.h> 
+#include <sys/poll.h> 
+#include <arpa/inet.h> 
+
+#endif // SYSTEM_HEADERS_H
